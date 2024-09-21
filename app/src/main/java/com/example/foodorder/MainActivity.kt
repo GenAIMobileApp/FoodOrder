4afa039ed3be3f671fef9eaa8ec317a3ddb3fd13
Pranay Airan <pranay.airan@iiitb.net>
2024-09-20 23:56:50 -0700

    removing the screens
    removing the screens

--- app/src/main/java/com/example/foodorder/MainActivity.kt
+++ app/src/main/java/com/example/foodorder/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.foodorder
 
-import NearbyRestaurantCard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,19 +9,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.foodorder.ui.theme.FoodOrderTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.foodorder.components.FoodChipComponent
-import com.example.foodorder.components.FoodItem
+import com.example.foodorder.ui.theme.FoodOrderTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,43 +44,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     FoodOrderTheme {
         Greeting("Android")
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun FoodAppComponentsPreview() {
-    MaterialTheme {
-        Surface {
-            Column {
-                Row (horizontalArrangement = Arrangement.SpaceEvenly) {
-                    FoodChipComponent(R.drawable.burger, "Burger")
-                    FoodChipComponent(R.drawable.chicken_fingers, "Roasted ")
-                }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                NearbyRestaurantCard(
-                    imageResId = R.drawable.taco,
-                    name = "Egg Benedict with Capsicum",
-                    distance = "2.2 away from you",
-                    rating = 4.9f,
-                    reviews = 1100
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                FoodItem(
-                    name = "Eggs Benedict with Capsicum",
-                    distance = "2.2",
-                    rating = 4.9f,
-                    reviews = 1100,
-                    price = "\$20.00",
-                    isFavorite = true
-                )
-            }
-        }
     }
 }
 
